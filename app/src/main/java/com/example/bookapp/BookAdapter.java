b9340e8db8f8c129a6938286cbfed1be63582ff0
Mauricio Fullana <mfullana4@gmail.com>
2020-10-25 16:45:49 -0300

    BookAdapter getView implementation. Item layout.

--- app/src/main/java/com/example/bookapp/BookAdapter.java
+++ app/src/main/java/com/example/bookapp/BookAdapter.java
@@ -1,16 +1,19 @@
 package com.example.bookapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class BookAdapter extends ArrayAdapter {
+public class BookAdapter extends ArrayAdapter<Book> {
 
     public BookAdapter(@NonNull Context context, int resource, ArrayList<Book> books) {
         super(context, resource, books);
@@ -19,6 +22,18 @@ public class BookAdapter extends ArrayAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View v = layoutInflater.inflate(R.layout.item_book, parent, false);
+
+        TextView txtBookName = v.findViewById(R.id.txt_book_name);
+        TextView txtAuthorName = v.findViewById(R.id.txt_book_author);
+        ImageView ivBookImage = v.findViewById(R.id.iv_book_image);
+
+        Book book = getItem(position);
+        txtBookName.setText(book.getBookName());
+        txtAuthorName.setText(book.getAuthorName());
+        ivBookImage.setImageDrawable(getContext().getResources().getDrawable(book.getBookImage()));
+
+        return v;
     }
 }
--- app/src/main/java/com/example/bookapp/MainActivity.java
+++ app/src/main/java/com/example/bookapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.bookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
     private ArrayList<Book> books;
+    private BookAdapter bookAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
         books.add(new Book("The Power of Habit", "Charles Duhigg", R.drawable.power_of_habbit));
         books.add(new Book("Rich Dad Poor Dad", "Robert Kiyosaki", R.drawable.rich_dad_poor_dad));
 
-
+        bookAdapter = new BookAdapter(this, R.layout.item_book, books);
+        listView.setAdapter(bookAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, books.get(position).getBookName()+"\n"+books.get(position).getAuthorName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
